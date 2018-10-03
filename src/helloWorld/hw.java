cc4353f91484f3c563067b6c3f626c22c21b29db
Gairik Acharya <gairik.acharya@us.ibm.com@Gairiks-MacBook-Pro.local>
2018-10-03 00:50:34 -0400

    Initial version of HelloWorld

---
+++ src/helloWorld/hw.java
@@ -0,0 +1,8 @@
+package helloWorld;
+public class hw {
+
+    // method main(): ALWAYS the APPLICATION entry point
+    public static void main (String[] args) {
+	System.out.println ("Hello World!");
+    }
+}
